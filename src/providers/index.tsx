1fbf1aebcac93f3eb36d00c6582aa685a5a55481
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 23:53:12 +0300

    feat: make violet the primary color

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,15 +1,19 @@
 import type { ReactNode } from "react";
 import { UserProvider } from "./user/Provider";
-import { MantineProvider } from "@mantine/core";
+import { createTheme, MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { BrowserRouter } from "react-router";
 
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 
+const theme = createTheme({
+  primaryColor: "violet",
+});
+
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <MantineProvider defaultColorScheme="auto">
+    <MantineProvider defaultColorScheme="auto" theme={theme}>
       <Notifications />
       <UserProvider>
         <BrowserRouter>{children}</BrowserRouter>
