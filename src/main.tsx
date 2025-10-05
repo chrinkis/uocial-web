df0e8b3e2ac3fb90165116bc279508a33ccfc48b
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 17:30:55 +0300

    feat: configure axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import axios from "axios";
 
 const root = document.getElementById("root");
 if (!root) {
   throw new Error();
 }
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
 createRoot(root).render(
   <StrictMode>
