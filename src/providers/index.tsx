f0e1151b3975087bcd183ab6c0e93b44ff314d4d
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 17:57:39 +0300

    feat: use MantineProvider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,13 @@
 import type { ReactNode } from "react";
 import { UserProvider } from "./user/Provider";
+import { MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
 
 export function AppProviders({ children }: { children: ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
+  return (
+    <MantineProvider>
+      <UserProvider>{children}</UserProvider>
+    </MantineProvider>
+  );
 }
