607dfc682ba8ce3f8256fd79fecfe7ad52d59952
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 21:45:21 +0300

    feat: add auth/login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router";
+import LoginPage from "@/pages/auth/login/page";
+
 function App() {
-  return "Hello, world!";
+  return (
+    <Routes>
+      <Route path="auth/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
