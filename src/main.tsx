5a2cad0b0bcfd6b39e2bc0bf228a7b6cdea2394e
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 17:33:58 +0300

    feat: fetch csrf-cookie on startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import axios from "axios";
+import { useAsync } from "react-use";
 
 const root = document.getElementById("root");
 if (!root) {
@@ -11,8 +12,24 @@ if (!root) {
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+export function Main() {
+  const { loading, error } = useAsync(
+    async () => await axios.get("/sanctum/csrf-cookie"),
+  );
+
+  if (loading) {
+    return; // FIXME
+  }
+
+  if (error) {
+    return; // FIXME
+  }
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+createRoot(root).render(<Main />);
