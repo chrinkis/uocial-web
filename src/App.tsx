158625de8a0df22e053e248539c91349e3430d2c
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 21:47:54 +0300

    feat: add index route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router";
+import IndexPage from "@/pages/page";
 import IndexLayout from "@/pages/layout";
+import AboutPage from "@/pages/about/page";
+import AppPage from "@/pages/app/page";
 import LoginPage from "@/pages/auth/login/page";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexLayout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="app" element={<AppPage />} />
         <Route path="auth/login" element={<LoginPage />} />
       </Route>
     </Routes>
---
+++ src/pages/page.tsx
@@ -0,0 +1,12 @@
+import { useUser } from "@/providers/user/hook";
+import { Navigate } from "react-router";
+
+export default function Page() {
+  const { user } = useUser();
+
+  if (!user) {
+    return <Navigate to="/about" />;
+  }
+
+  return <Navigate to="/app" />;
+}
