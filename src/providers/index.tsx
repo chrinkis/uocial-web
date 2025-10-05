1e0e4e295dfacf20a7afde520a5e5b56cef391c1
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-05 19:48:49 +0300

    feat: use Notifications provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,12 +1,15 @@
 import type { ReactNode } from "react";
 import { UserProvider } from "./user/Provider";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <MantineProvider>
+      <Notifications />
       <UserProvider>{children}</UserProvider>
     </MantineProvider>
   );
