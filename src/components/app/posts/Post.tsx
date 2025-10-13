60c5d487e6500fdad459e00c7dda1acdfa6850e8
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-13 20:18:16 +0300

    feat: add Post component

---
+++ src/components/app/posts/Post.tsx
@@ -0,0 +1,191 @@
+import * as post from "@/models/app/post/Post";
+import {
+  Group,
+  Paper,
+  Stack,
+  Title,
+  Text,
+  Spoiler,
+  Tooltip,
+  Badge,
+  Anchor,
+  UnstyledButton,
+} from "@mantine/core";
+import {
+  IconArrowBigDown,
+  IconArrowBigDownFilled,
+  IconArrowBigUp,
+  IconArrowBigUpFilled,
+  IconClock,
+  IconMessageCircle,
+} from "@tabler/icons-react";
+import { format, formatDistanceToNow } from "date-fns";
+import { NavLink } from "react-router";
+
+export interface PostPropsType {
+  post: post.Post;
+}
+
+function PostTitle({ post }: PostPropsType) {
+  return (
+    <Spoiler
+      showLabel="Show more"
+      hideLabel="Show less"
+      style={{ flex: 1 }}
+      maxHeight={96}
+      styles={{
+        control: {
+          textAlign: "right",
+          width: "100%",
+        },
+      }}
+    >
+      <Title order={3} ta="justify">
+        {post.title}
+      </Title>
+    </Spoiler>
+  );
+}
+
+function PostMetaData({ post }: PostPropsType) {
+  const createdAt = new Date(post.created_at);
+
+  return (
+    <Group justify="space-between" wrap="nowrap">
+      <Group wrap="wrap" style={{ flex: 1 }}>
+        <Badge
+          size="md"
+          variant="gradient"
+          gradient={{ from: "grape", to: "violet", deg: 90 }}
+        >
+          !{post.id}
+        </Badge>
+
+        {post.labels.map((l) => (
+          <Badge
+            key={l.value}
+            size="md"
+            variant="gradient"
+            gradient={{ from: "grape", to: "violet", deg: 90 }}
+          >
+            {l.value}
+          </Badge>
+        ))}
+      </Group>
+
+      <Group justify="flex-end" gap={5} wrap="nowrap">
+        <IconClock color="var(--mantine-color-dimmed)" size="1.2rem" />
+        <Tooltip
+          label={format(createdAt, "PP·p")}
+          events={{ touch: true, hover: true, focus: false }}
+        >
+          <Text c="dimmed">
+            {formatDistanceToNow(createdAt, { addSuffix: true })}
+          </Text>
+        </Tooltip>
+      </Group>
+    </Group>
+  );
+}
+
+function PostBody({ post }: PostPropsType) {
+  return (
+    <Spoiler
+      showLabel="Show more"
+      hideLabel="Show less"
+      style={{ flex: 1 }}
+      maxHeight={200}
+      styles={{
+        control: {
+          textAlign: "right",
+          width: "100%",
+        },
+      }}
+    >
+      <Text ta="justify">{post.body}</Text>
+    </Spoiler>
+  );
+}
+
+function PostHashtags({ post }: PostPropsType) {
+  return (
+    <Spoiler
+      showLabel="Show more"
+      hideLabel="Show less"
+      style={{ flex: 1 }}
+      maxHeight={75}
+      styles={{
+        control: {
+          textAlign: "right",
+          width: "100%",
+        },
+      }}
+    >
+      <Group gap="0.5rem" style={{ rowGap: 0 }}>
+        {post.hashtags.map((h) => (
+          <Anchor component={NavLink} key={h.value} to="">
+            #{h.value}
+          </Anchor>
+        ))}
+      </Group>
+    </Spoiler>
+  );
+}
+
+function PostReactions({ post }: PostPropsType) {
+  return (
+    <Group justify="space-between">
+      <Group>
+        <Group gap={2} align="stretch">
+          {post.reactions.user?.reaction === "Upvote" ? (
+            <UnstyledButton c="violet">
+              <IconArrowBigUpFilled />
+            </UnstyledButton>
+          ) : (
+            <UnstyledButton>
+              <IconArrowBigUp />
+            </UnstyledButton>
+          )}
+
+          <Text>{post.reactions.total.upvotes}</Text>
+        </Group>
+
+        <Group gap={2} align="stretch">
+          <Text>{post.reactions.total.downvotes}</Text>
+
+          {post.reactions.user?.reaction === "Downvote" ? (
+            <UnstyledButton c="violet">
+              <IconArrowBigDownFilled />
+            </UnstyledButton>
+          ) : (
+            <UnstyledButton>
+              <IconArrowBigDown />
+            </UnstyledButton>
+          )}
+        </Group>
+      </Group>
+
+      <Group gap={2} align="stretch">
+        <Text>{post.comments.total}</Text>
+
+        <UnstyledButton>
+          <IconMessageCircle />
+        </UnstyledButton>
+      </Group>
+    </Group>
+  );
+}
+
+export function Post(props: PostPropsType) {
+  return (
+    <Paper w="90%" p="md" maw="50rem" withBorder>
+      <Stack>
+        <PostTitle {...props} />
+        <PostMetaData {...props} />
+        <PostBody {...props} />
+        <PostHashtags {...props} />
+        <PostReactions {...props} />
+      </Stack>
+    </Paper>
+  );
+}
